24215a74d3c9785fc3dbc02a0b409c6c65a56208
Zach Pistole <z.pistole@gmail.com>
2020-04-06 22:07:59 -0700

    first commit

--- _/components/js/image-swap.js
+++ _/components/js/image-swap.js
@@ -1,47 +1,79 @@
-$('img').on('dragstart', function(event) { event.preventDefault(); });
-$('document').ready(function() {
-  $('img.swap').on({
-    'mouseover' : function() {
-      $(this).attr('src','http://www.playcraftsystems.com/staging/passport/img/rocket-back.png');
+$("img").on("dragstart", function (event) {
+  event.preventDefault();
+});
+$("document").ready(function () {
+  $("img.swap").on({
+    mouseover: function () {
+      $(this).attr(
+        "src",
+        "http://www.zpistolerivers.com/passport-final/img/lg-imgs/rocket-back.png"
+      );
     },
-    mouseout : function() {
-  $(this).attr('src','http://www.playcraftsystems.com/staging/passport/img/playcraft-systems-passport-rocket.jpg');
-    }
+    mouseout: function () {
+      $(this).attr(
+        "src",
+        "http://www.zpistolerivers.com/passport-final/img/lg-imgs/playcraft-systems-passport-rocket.jpg"
+      );
+    },
   }); //END ROCKET IMAGE
 
-  $('img.swap2').on({
-    'mouseover' : function() {
-      $(this).attr('src','http://www.playcraftsystems.com/staging/passport/img/locomotive-back.png');
+  $("img.swap2").on({
+    mouseover: function () {
+      $(this).attr(
+        "src",
+        "http://www.zpistolerivers.com/passport-final/img/lg-imgs/locomotive-back.png"
+      );
     },
-    mouseout : function() {
-  $(this).attr('src','http://www.playcraftsystems.com/staging/passport/img/playcraft-systems-passport-train.jpg');
-    }
+    mouseout: function () {
+      $(this).attr(
+        "src",
+        "http://www.zpistolerivers.com/passport-final/img/lg-imgs/playcraft-systems-passport-train.jpg"
+      );
+    },
   }); //END TRAIN THEME
 
-  $('img.swap3').on({
-    'mouseover' : function() {
-      $(this).attr('src','http://www.playcraftsystems.com/staging/passport/img/western-back.png');
+  $("img.swap3").on({
+    mouseover: function () {
+      $(this).attr(
+        "src",
+        "http://www.zpistolerivers.com/passport-final/img/lg-imgs/western-back.png"
+      );
     },
-    mouseout : function() {
-  $(this).attr('src','http://www.playcraftsystems.com/staging/passport/img/playcraft-systems-passport-western.jpg');
-    }
+    mouseout: function () {
+      $(this).attr(
+        "src",
+        "http://www.zpistolerivers.com/passport-final/img/lg-imgs/playcraft-systems-passport-western.jpg"
+      );
+    },
   }); //END WESTERN THEME
 
-  $('img.swap4').on({
-    'mouseover' : function() {
-      $(this).attr('src','http://www.playcraftsystems.com/staging/passport/img/castle-back.png');
+  $("img.swap4").on({
+    mouseover: function () {
+      $(this).attr(
+        "src",
+        "http://www.zpistolerivers.com/passport-final/img/lg-imgs/castle-back.png"
+      );
     },
-    mouseout : function() {
-  $(this).attr('src','http://www.playcraftsystems.com/staging/passport/img/playcraft-systems-passport-castle.jpg');
-    }
+    mouseout: function () {
+      $(this).attr(
+        "src",
+        "http://www.zpistolerivers.com/passport-final/img/lg-imgs/playcraft-systems-passport-castle.jpg"
+      );
+    },
   }); //END CASTLE THEME
 
-  $('img.swap5').on({
-    'mouseover' : function() {
-      $(this).attr('src','http://www.playcraftsystems.com/staging/passport/img/pirate-back.png');
+  $("img.swap5").on({
+    mouseover: function () {
+      $(this).attr(
+        "src",
+        "http://www.zpistolerivers.com/passport-final/img/lg-imgs/pirate-back.png"
+      );
     },
-    mouseout : function() {
-  $(this).attr('src','http://www.playcraftsystems.com/staging/passport/img/playcraft-systems-passport-pirate.jpg');
-    }
+    mouseout: function () {
+      $(this).attr(
+        "src",
+        "http://www.zpistolerivers.com/passport-final/img/lg-imgs/playcraft-systems-passport-pirate.jpg"
+      );
+    },
   }); //END PIRATE THEME
 });
--- includes/postcards.php
+++ includes/postcards.php
@@ -6,13 +6,13 @@
   <!-- ========ROCKET POSTCARD======== -->
   <div class="col-md-4 padding2">
     <a href="rocket.php" onClick="ga('send', 'event', 'Rocket Card', 'click', 'Passport Promo');">
-      <img class="swap" src="http://www.playcraftsystems.com/staging/passport/img/playcraft-systems-passport-rocket.jpg" alt="" />
+      <img class="swap" src="img/playcraft-systems-passport-rocket.jpg" alt="" />
     </a>
   </div>
   <!-- ========LOCOMOTIVE POSTCARD======== -->
   <div class="col-md-4 padding2">
     <a href="locomotive.php" onClick="ga('send', 'event', 'Train Card', 'click', 'Passport Promo');">
-      <img class="swap2" src="http://www.playcraftsystems.com/staging/passport/img/playcraft-systems-passport-train.jpg" alt="" />
+      <img class="swap2" src="img/playcraft-systems-passport-train.jpg" alt="" />
     </a>
   </div>
 </div>
@@ -20,19 +20,19 @@
   <!-- ========WESTERN POSTCARD======== -->
   <div class="col-md-4 padding2">
     <a href="western.php" onClick="ga('send', 'event', 'Western Card', 'click', 'Passport Promo');">
-      <img class="swap3" src="http://www.playcraftsystems.com/staging/passport/img/playcraft-systems-passport-western.jpg" alt="" />
+      <img class="swap3" src="img/playcraft-systems-passport-western.jpg" alt="" />
     </a>
   </div>
   <!-- ========CASTLE POSTCARD======== -->
   <div class="col-md-4 padding2">
     <a href="castle.php" onClick="ga('send', 'event', 'Castle Card', 'click', 'Passport Promo');">
-      <img class="swap4" src="http://www.playcraftsystems.com/staging/passport/img/playcraft-systems-passport-castle.jpg" alt="" />
+      <img class="swap4" src="img/playcraft-systems-passport-castle.jpg" alt="" />
     </a>
   </div>
   <!-- ========PIRATE POSTCARD======== -->
   <div class="col-md-4 padding2">
     <a href="pirate.php" onClick="ga('send', 'event', 'Pirate Card', 'click', 'Passport Promo');">
-      <img class="swap5" src="http://www.playcraftsystems.com/staging/passport/img/playcraft-systems-passport-pirate.jpg" alt="" />
+      <img class="swap5" src="img/playcraft-systems-passport-pirate.jpg" alt="" />
     </a>
   </div>
 </div>
